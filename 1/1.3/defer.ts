d0196bb71b31be59d6b9de07e2a84e2e45bcadf2
mtraub <mario.traub@ergon.ch>
2024-08-21 14:14:12 +0200

    1.3 is now typescript

--- 1/1.3/defer.ts
+++ 1/1.3/defer.ts
@@ -1,8 +1,8 @@
-"use strict";
 // 3
 // innerHTML instead of innerText render the button correctly
 document.body.innerHTML += "Hello World!";
+
 // 4
-document.getElementById("button").onclick = () => {
+(document.getElementById("button") as HTMLButtonElement).onclick = () => {
     alert("Hello World!");
 };
