ba7a3b1a1da4ce5915213d59eafbf99ad87ff5c4
mtraub <mario.traub@ergon.ch>
2024-08-21 13:49:31 +0200

    1.6

---
+++ 1/1.6/script.ts
@@ -0,0 +1,6 @@
+const userName = prompt("Wie heisst du?");
+const hours = new Date().getHours();
+
+const dayTime = hours < 17 ? "Tag" : hours < 22 ? "Abend" : "Nacht";
+
+alert(`Guten ${dayTime}, ${userName}!`);
