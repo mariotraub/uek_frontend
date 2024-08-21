568cca00bd83dd61009e3de5dbe4c904fc469dc3
mtraub <mario.traub@ergon.ch>
2024-08-21 11:41:01 +0200

    1.3

---
+++ 1/1.3/defer.js
@@ -0,0 +1,8 @@
+// 3
+// innerHTML instead of innerText render the button correctly
+document.body.innerHTML += "Hello World!";
+
+// 4
+document.getElementById("button").onclick = () => {
+    alert("Hello World!");
+};
