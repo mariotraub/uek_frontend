32b0f4de17303685deb05c2d880b7849f7fa9966
mtraub <mario.traub@ergon.ch>
2024-08-23 11:00:23 +0200

    3.4

---
+++ 3/3.4/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const container = document.getElementById("destinations");
+const choose = document.getElementById("choose");
+const destinations = [].slice.call(container.children);
+choose.onclick = selectRandomDestination;
+function selectRandomDestination() {
+    // Remove the selected class from all destinations
+    destinations.forEach(destination => destination.classList.remove("selected"));
+    const randomIndex = Math.floor(Math.random() * destinations.length);
+    destinations[randomIndex].classList.add("selected");
+}
---
+++ 3/3.4/script.ts
@@ -0,0 +1,12 @@
+const container = document.getElementById("destinations")!!;
+const choose = document.getElementById("choose")!!;
+const destinations: HTMLLIElement[] = [].slice.call(container.children);
+
+choose.onclick = selectRandomDestination;
+function selectRandomDestination() {
+    // Remove the selected class from all destinations
+    destinations.forEach(destination => destination.classList.remove("selected"));
+
+    const randomIndex = Math.floor(Math.random() * destinations.length);
+    destinations[randomIndex].classList.add("selected");
+}
