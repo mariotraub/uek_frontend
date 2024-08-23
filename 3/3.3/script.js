efc47507fc342c92bcc7fb4d20d733d114472db9
mtraub <mario.traub@ergon.ch>
2024-08-23 10:42:25 +0200

    finish 3.3

--- 3/3.3/script.js
+++ 3/3.3/script.js
@@ -6,23 +6,32 @@ const form = document.querySelector('form');
 const blackList = ['mousemove', 'mouseout'];
 
 function logEvent(name, origin, value) {
+  // Erstellt ein p
   const message = document.createElement('p');
+  // Wenn der Wert existiert, wird der Wert mit ausgegeben
   if(value) {
     message.innerText = `${counter}: "${name}"-Ereignis durch ${origin} mit dem Wert ${value} ausgelöst.`;
   } else {
     message.innerText = `${counter}: "${name}"-Ereignis durch ${origin} ausgelöst.`;
   }
+  // Fügt das p an den Anfang des Logs
   log.prepend(message);
+  // Erhöht den Counter um 1
   counter++;
 }
 
 function registerEvents(element) {
+  // Durchläuft alle Keys des Elements
   for(const key in element) {
+    // Regex zum überprüfen ob der Key mit "on" beginnt
     if (/^on/.test(key)) {
+      // Entfernt das "on" vom Key und speichert den Namen
       const eventName = key.slice(2);
+      // Wenn die Blacklist den Eventnamen enthält, wird es der loop abgebrochen
       if(blackList.includes(eventName)) {
         break;
       }
+      // Fügt einen EventListener hinzu, der das Event loggt
       window.addEventListener(eventName, (e) => {
         logEvent(eventName, e.target.nodeName ,e.target.value);
       });
@@ -30,6 +39,7 @@ function registerEvents(element) {
   };
 }
 
+// Regestiert alle Events für window
 registerEvents(window);
 
 // Verhindern, dass die Seite beim Abschicken des Formulars neu lädt und so die Logs verschwinden.
