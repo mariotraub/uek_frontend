e0f651a83609f6dc1fa566efc53b2cb969ee66b4
mtraub <mario.traub@ergon.ch>
2024-08-23 16:39:16 +0200

    4.3

---
+++ 4/4.3/script.js
@@ -0,0 +1,25 @@
+"use strict";
+const canvas = document.getElementById('smiley');
+const context = canvas.getContext("2d");
+// Kopf zeichnen
+context.beginPath();
+context.arc(100, 100, 100, 0, 360);
+context.stroke();
+// Augen zeichnen
+context.beginPath();
+context.arc(75, 75, 10, 0, 360);
+context.stroke();
+context.beginPath();
+context.arc(75, 75, 1, 0, 360);
+context.stroke();
+context.beginPath();
+context.arc(125, 75, 10, 0, 360);
+context.stroke();
+context.beginPath();
+context.arc(125, 75, 1, 0, 360);
+context.stroke();
+// Mund zeichnen
+context.beginPath();
+context.arc(100, 75, 70, Math.PI - 1, 2 * Math.PI + 1, true);
+context.closePath();
+context.stroke();
