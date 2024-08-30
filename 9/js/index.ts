6fa3b1bbfa1c8b75eabec9cff0deaf64055f4c87
mtraub <mario.traub@ergon.ch>
2024-08-30 16:40:13 +0200

    Projekt read

---
+++ 9/js/index.ts
@@ -0,0 +1,25 @@
+import type { Task } from "./types";
+
+const todoContainer = document.getElementById("todo-container")!;
+
+renderTodos();
+
+export async function renderTodos() {
+    const todos = await (await fetch("http://localhost/tasks")).json() as Task[];
+    todos.forEach(todo => {
+        const task = document.createElement("div");
+        task.classList.add("task");
+        const completed = document.createElement("div");
+        completed.classList.add(todo.completed ? "complete" : "incomplete");
+        completed.classList.add("checkbox");
+        const title = document.createElement("h2");
+        title.classList.add("title");
+
+        title.innerText = todo.title;
+
+        task.appendChild(completed);
+        task.appendChild(title);
+
+        todoContainer.appendChild(task);
+    });
+}
